4aae4800ac41c0781e686b7f92cfc9dbbc59434f
NeverC <xzllc2010@gmail.com>
2014-04-01 17:50:04 +0800

    Add InsertSort class

---
+++ src/never/data_structure/sort/InsertSort.java
@@ -0,0 +1,15 @@
+/**InsertSort.java
+ * never.data_structure.sort
+ * Apr 1, 2014 5:46:05 PM 
+ * xzllc2010@gmail.com
+ */
+package never.data_structure.sort;
+
+public class InsertSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
