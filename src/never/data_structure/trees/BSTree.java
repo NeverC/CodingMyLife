ec9fe1b142a3615d5ba2e47737159522cb37bbf7
NeverC <xzllc2010@gmail.com>
2014-04-06 22:05:17 +0800

    add BSTree class but not complete

---
+++ src/never/data_structure/trees/BSTree.java
@@ -0,0 +1,12 @@
+/**  
+ * @Title: BSTree.java
+ * @Package never.data_structure.trees
+ * @author "Never" xzllc2010#gmail.com  
+ * @date Apr 5, 2014 10:19:24 AM
+ * @Description: Binary Search Tree
+ */
+package never.data_structure.trees;
+
+public class BSTree {
+
+}
